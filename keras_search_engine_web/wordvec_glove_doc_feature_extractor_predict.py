7a9ea6b096733d150d82a82fda87acd6dd00f28d
xschen <xs0040@gmail.com>
2017-11-18 15:05:43 +0800

    implement the feature extractor for the doc glove

--- keras_search_engine_web/wordvec_glove_doc_feature_extractor_predict.py
+++ keras_search_engine_web/wordvec_glove_doc_feature_extractor_predict.py
@@ -2,40 +2,92 @@ from keras.models import model_from_json
 import numpy as np
 from keras.preprocessing.sequence import pad_sequences
 import nltk
+import os
+import sys
+import zipfile
+import urllib.request
+
+EMBED_SIZE = 100
+GLOVE_MODEL = "../keras_search_engine_train/very_large_data/glove.6B." + str(EMBED_SIZE) + "d.txt"
+
+def reporthook(block_num, block_size, total_size):
+    read_so_far = block_num * block_size
+    if total_size > 0:
+        percent = read_so_far * 1e2 / total_size
+        s = "\r%5.1f%% %*d / %d" % (
+            percent, len(str(total_size)), read_so_far, total_size)
+        sys.stderr.write(s)
+        if read_so_far >= total_size:  # near the end
+            sys.stderr.write("\n")
+    else:  # total size is unknown
+        sys.stderr.write("read %d\n" % (read_so_far,))
 
 
-class WordVecBidirectionalLstmSoftmax(object):
+def download_glove():
+    if not os.path.exists(GLOVE_MODEL):
+        if not os.path.exists('../keras_search_engine_train/very_large_data'):
+            os.makedirs('../keras_search_engine_train/very_large_data')
+
+        glove_zip = '../keras_search_engine_train/very_large_data/glove.6B.zip'
+
+        if not os.path.exists(glove_zip):
+            print('glove file does not exist, downloading from internet')
+            urllib.request.urlretrieve(url='http://nlp.stanford.edu/data/glove.6B.zip', filename=glove_zip,
+                                       reporthook=reporthook)
+
+        print('unzipping glove file')
+        zip_ref = zipfile.ZipFile(glove_zip, 'r')
+        zip_ref.extractall('../keras_search_engine_train/very_large_data')
+        zip_ref.close()
+
+
+def load_glove():
+    _word2em = {}
+    file = open(GLOVE_MODEL, mode='rt', encoding='utf8')
+    for line in file:
+        words = line.strip().split()
+        word = words[0]
+        embeds = np.array(words[1:], dtype=np.float32)
+        _word2em[word] = embeds
+    file.close()
+    return _word2em
+
+
+class WordVecGloveDocFeatureExtractor(object):
     model = None
-    word2idx = None
-    idx2word = None
+    word2em = None
     context = None
 
     def __init__(self):
-        json = open('../keras_search_engine_train/models/bidirectional_lstm_softmax_architecture.json', 'r').read()
+        self.word2em = load_glove()
+        json = open('../keras_search_engine_train/models/glove_fe_architecture.json', 'r').read()
         self.model = model_from_json(json)
-        self.model.load_weights('../keras_search_engine_train/models/bidirectional_lstm_softmax_weights.h5')
-        self.model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
-        self.word2idx = np.load('../keras_search_engine_train/models/umich_idx2word_bidirectional_lstm.npy').item()
-        self.idx2word = np.load('../keras_search_engine_train/models/umich_word2idx_bidirectional_lstm.npy').item()
-        self.context = np.load('../keras_search_engine_train/models/umich_context_bidirectional_lstm.npy').item()
+        self.model.load_weights('../keras_search_engine_train/models/glove_fe_weights.h5')
+        self.model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+        self.context = np.load('../keras_search_engine_train/models/umich_context_glove.npy').item()
 
-    def predict(self, sentence):
-        xs = []
+    def extract(self, sentence):
         max_len = self.context['maxlen']
         tokens = [w.lower() for w in nltk.word_tokenize(sentence)]
-        wid = [self.word2idx[token] if token in self.word2idx else 1 for token in tokens]
-        xs.append(wid)
-        x = pad_sequences(xs, max_len)
-        output = self.model.predict(x)
-        neg, pos = output[0]
-        return [pos, neg]
+
+        X = np.zeros(shape=EMBED_SIZE)
+        E = np.zeros(shape=(EMBED_SIZE, max_len))
+        for j in range(0, len(tokens) - 1):
+            word = tokens[j]
+            try:
+                E[:, j] = self.word2em[word]
+            except KeyError:
+                pass
+        X[:] = np.sum(E, axis=1)
+        output = self.model.predict(X)
+        negative, positive = output[0]
+        return [positive, negative]
 
     def test_run(self, sentence):
-        result = self.predict(sentence)
-        print(result)
+        print(self.predict(sentence))
 
 
 if __name__ == '__main__':
-    app = WordVecBidirectionalLstmSoftmax()
+    app = WordVecGloveDocFeatureExtractor()
     app.test_run('i liked the Da Vinci Code a lot.')
-    app.test_run('I hate friday')
+    app.test_run('i hated the Da Vinci Code a lot.')
--- keras_search_engine_web/wordvec_glove_feature_extractor.py
+++ keras_search_engine_web/wordvec_glove_feature_extractor.py
@@ -1,12 +1,11 @@
-from keras.models import model_from_json
 import numpy as np
-from keras.preprocessing.sequence import pad_sequences
 import nltk
 import os
 import sys
 import zipfile
 import urllib.request
 
+MAX_SEQ_LENGTH = 3000
 EMBED_SIZE = 100
 GLOVE_MODEL = "../keras_search_engine_train/very_large_data/glove.6B." + str(EMBED_SIZE) + "d.txt"
 
@@ -42,35 +41,31 @@ def download_glove():
 
 
 def load_glove():
-    _word2em = {}
+    download_glove()
+    word2em = {}
     file = open(GLOVE_MODEL, mode='rt', encoding='utf8')
     for line in file:
         words = line.strip().split()
         word = words[0]
         embeds = np.array(words[1:], dtype=np.float32)
-        _word2em[word] = embeds
+        word2em[word] = embeds
     file.close()
-    return _word2em
+    return word2em
 
 
 class WordVecGloveFeatureExtractor(object):
-    model = None
     word2em = None
     context = None
 
     def __init__(self):
         self.word2em = load_glove()
-        json = open('../keras_search_engine_train/models/glove_fe_architecture.json', 'r').read()
-        self.model = model_from_json(json)
-        self.model.load_weights('../keras_search_engine_train/models/glove_fe_weights.h5')
-        self.model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-        self.context = np.load('../keras_search_engine_train/models/umich_context_glove.npy').item()
+        self.context = dict()
+        self.context['maxlen'] = MAX_SEQ_LENGTH
 
     def extract(self, sentence):
         max_len = self.context['maxlen']
         tokens = [w.lower() for w in nltk.word_tokenize(sentence)]
 
-        X = np.zeros(shape=(1, EMBED_SIZE))
         E = np.zeros(shape=(EMBED_SIZE, max_len))
         for j in range(0, len(tokens) - 1):
             word = tokens[j]
@@ -78,13 +73,28 @@ class WordVecGloveFeatureExtractor(object):
                 E[:, j] = self.word2em[word]
             except KeyError:
                 pass
-        X[0, :] = np.sum(E, axis=1)
-        output = self.model.predict(X)
-        negative, positive = output[0]
-        return [positive, negative]
+        return np.sum(E, axis=1)
+
+    def extract_all(self, sentences):
+        doc_count = len(sentences)
+        max_len = self.context['maxlen']
+
+        result = []
+        for k in range(doc_count-1):
+            tokens = [w.lower() for w in nltk.word_tokenize(sentences[k])]
+
+            E = np.zeros(shape=(EMBED_SIZE, max_len))
+            for j in range(0, len(tokens) - 1):
+                word = tokens[j]
+                try:
+                    E[:, j] = self.word2em[word]
+                except KeyError:
+                    pass
+            result.append(np.sum(E, axis=1))
+        return result
 
     def test_run(self, sentence):
-        print(self.predict(sentence))
+        print(self.extract(sentence))
 
 
 if __name__ == '__main__':
